0a4ce7ff2a8d3541493aac5f3c2c6dff2e6f525e
Napageneral <Tnapathy@gmail.com>
2019-01-05 20:44:42 -0800

    Initial Commit
    Laying the groundwork for the python rebuild.

---
+++ Execute.py
@@ -0,0 +1,48 @@
+
+
+#Create main execution sub
+#parameters: Analytics engine, datasource, targetfields, includedfields, model key
+
+
+if __name__ = '__main__':
+    main()
+
+def main(AnalyticsEngine, DataSource, TargetFields, IncludedFields, ModelKey):
+
+    #Eventual HyperHyperparameters
+    #NumberOfFolds = 5
+    #OptimizationIterations = 1
+    #UndersamplingPercentage = 100
+    #OversamplingPercentage = 0
+    #ObjectiveFunction = New TotalErrorClassificationMetric()
+
+
+    #Generate Dataset from CSV
+
+
+
+    #Process/Clean Dataset
+
+
+
+    #Train Model
+
+
+
+    #Test Model
+
+
+
+    #Generate Statistics
+
+
+
+    #Train Final Model
+
+
+
+    #Compress Final Model
+
+
+
+    #Store Results
